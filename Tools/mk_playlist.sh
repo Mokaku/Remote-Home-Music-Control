3f32d452791d3312441f0e0d6d26c37dc5abdcf5
Mokaku <mokaku@gmail.com>
2011-02-25 14:51:59 +0900

    Delete teet code from mk_playlist.sh

--- Tools/mk_playlist.sh
+++ Tools/mk_playlist.sh
@@ -13,20 +13,6 @@ fi
 today=`date +%Y%m%d`
 p_list=playlist_${today}.m3u
 
-## test ------------
-## echo "test"
-## echo ${ap_dir}
-## echo ${cur_path}/${ap_dir}
-## if test -d ${cur_path}/${ap_dir}
-## then
-## echo "Yes! this Dir is Correct"
-## else
-## echo "Nooooop! This Dir is Uncorrect"
-## fi
-
-## end of test -----
-
-
 if test -d ${cur_path}/${ap_dir}
 then
 find ${cur_path}/${ap_dir} -name '*\.mp3' >> ${p_list}
